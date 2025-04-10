db73374376585195dcd516bc69c4c0249defb8ac
ctih <onni09nevala@gmail.com>
2025-04-10 18:44:38 +0300

    added server ready time indicator

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
 import sys
-
-print(os.curdir)
+import time
 
 load_dotenv()
 os.environ["ZONE_ID"] = os.environ.get("ZONEID") or os.environ.get("ZONE_ID")
 
+start = time.time()
 from server.main import app
+print(f"Server ready in {round(time.time() - start,2)} seconds")
 
 if "run" in sys.argv:
     import uvicorn
